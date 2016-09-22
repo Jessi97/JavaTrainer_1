cc9cea34935c918ebcd83672568a396cb8b3eb73
Jessi97 <jessica-schueller@web.de>
2016-09-22 11:31:19 +0200

    PersistenceSettings

---
+++ PersistenceSettings.java
@@ -0,0 +1,17 @@
+package JavaTrainer;
+
+public class PersistenceSettings {
+
+private String persistanceType;
+
+
+public String getPersistanceType(){
+	return persistanceType;
+}
+
+public void setPersistanceType(String persistanceType){
+	this.persistanceType = persistanceType; 
+}
+
+
+}
