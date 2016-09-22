378d4f05fdbce712f9e3e19204a23e44d941d469
Jessi97 <jessica-schueller@web.de>
2016-09-22 10:51:34 +0200

    Trainer - Business Object

---
+++ Trainer.java
@@ -0,0 +1,20 @@
+package JavaTrainer;
+
+import java.sql.Date;
+
+public class Trainer {
+	
+	private int id; 
+	private String nname; 
+	private String vname; 
+	private Date geburtstag; 
+	private int alter; 
+	private int erfahrung;
+	
+	public void setID(int id){
+		this.id = id; 
+	}
+	
+
+	
+}
